4a73f9121a8128cae08f4f4910030976d2ef384b
maplelover <278390341@qq.com>
2014-03-05 11:08:30 +0800

    modify main.js, print infomation in the console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+
+console.log('Hi, it is greate to meet you in the console!');
+console.log('You can concate me with QQ-278390341!');
+console.log('Thanks.');
+
+$(document).ready(function () {
+	alert('just for test javascript');
+})
